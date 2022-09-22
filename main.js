fbc0e65389dcc760f33cc3864392ee22c5dacd6f
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2022-09-21 20:57:27 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 12. Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en
-    //     caso contrario indicar con un mensaje que la operación no es posible realizarla
-    let num1= Number(prompt('Ingrese un numero al que restara'))
-    let num2= Number(prompt('Ingrese un numero a restar'))
-    if (num1>num2){
-        resta=num1-num2
-        console.log(`El resultado de la resta es ${resta}`);
-    }
-    else{
-        console.log('No es posible realizar la operacion');
-    }
+    // 13. En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendo
+    // de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C.
+    // Los descuentos son los siguientes:
+    // Tipo A 10% de descuento
+    // Tipo B 15% de descuento
+    // Tipo C 20% de descuento
+    
+   let helado = Number(prompt('Ingrese el valor del helado'))
+   let memb = prompt('Ingrese el tipo de membresia')
+        if (memb=='A'){
+            let desc=helado*0.10
+            let valor=helado-desc
+            console.log(`Es un total de ${valor}`);
+        }
+        else if (memb=='B'){
+            let desc=helado*0.15
+            let valor=helado-desc
+            console.log(`Es un total de ${valor}`);
+        }
+        else if (memb=='C'){
+            let desc=helado*0.20
+            let valor=helado-desc
+            console.log(`Es un total de ${valor}`);
+        }
+        else{
+            console.log(`No tiene membresia, el total es de ${helado}`);
+        }
 })
