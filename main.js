a4863eb9aa5df45eccb6525230d9b71d6c944b37
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2022-09-21 19:09:23 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 7. Calcular todos los pagos hechos de un restaurante y que si el consumo ingresado excede los
-    // $130.000 el descuento será del 15%, de lo contrario no hay descuento.
-    let descuento = 0.15
-    let suma = 0
-    do{
-        cuenta = Number(prompt('Insertar el valor de lo consumido'));
-        suma+=cuenta
+    // 8. En cierta empresa se les paga a sus trabajadores de la siguiente forma: si el empleado es de planta,
+    // la hora trabajada se le paga a $20000, si el empleado es administrativo, la hora trabajada se le paga
+    // a $10000. Para calcular su pago es necesario conocer el total de horas trabajadas.    
+    let horaPlanta = 20000
+    let horaAdmin = 10000
+    let horas = Number(prompt('Inserte la cantidad de horas trabajadas'))
+    let tipoEmple = prompt('Identifiquese como trabajador de planta o administrativo')
+    let minus = tipoEmple.toLowerCase
+    if (minus=='planta'){
+        pagaPlanta=horas*horaPlanta
+        console.log(`Su paga es de ${pagaPlanta}`);
     }
-    while(confirm('Desea ingresar otro producto'));
-
-    if (suma>130000){
-        desct=suma*descuento
-        final=suma-desct
-        console.log(`El total a pagar con descuento es de ${final}`);
+    else if (minus=='administrativo'){
+        pagaAdmin=horas*horaAdmin
+        console.log(`Su paga es de ${pagaAdmin}`);
     }
     else{
-        console.log(`El valor total a pagar es de ${suma}`);
+        console.log('No se identifico como un trabajador');
     }
 })
